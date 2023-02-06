0e468e8776fc9642ddaf594c4ec61ae40b7dcd7a
Ольга Казанцева <masqcat@gmail.com>
2023-02-06 21:08:52 +0300

    оно работает, но на работе просто include работать не будут

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('posts/', views.index, name="index"),
     #path('group/', views.group_posts),
-    path('group/<slug:slug>/', views.group_posts, name="group_posts"),
+    path('group/<slug:slug>/', views.group_posts, name="group_list"),
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,29 +1,33 @@
-from django.shortcuts import render
-#from django.http import HttpResponse 
+from django.shortcuts import render, get_object_or_404
 
-def index(request): 
-    template = 'posts/index.html'
+from .models import Post, Group
 
-    title = 'Это главная страница проекта Yatube'
-    # Словарь с данными принято называть context
+def index(request):
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        # В словарь можно передать переменную
-        'title': title,
-        # А можно сразу записать значение в словарь. Но обычно так не делают
-        'text': 'Это главная страница проекта Yatube',
+        'posts': posts,
     }
-
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context) 
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    title = 'Это не главная страница проекта Yatube'
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        # В словарь можно передать переменную
-        'title': title,
-        # А можно сразу записать значение в словарь. Но обычно так не делают
-        'text': 'Здесь будет информация о группах проекта Yatube',
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context) 
     
