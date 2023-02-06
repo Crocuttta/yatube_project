07dc1952fb75ad25598fad0a80b7bb2279087970
Ольга Казанцева <masqcat@gmail.com>
2023-02-06 16:41:30 +0300

    16:41

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+#from django.http import HttpResponse 
 
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request): 
+    template = 'posts/index.html'
+
+    title = 'Это главная страница проекта Yatube'
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Это главная страница проекта Yatube',
+    }
+
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Группа {slug}')
+    template = 'posts/group_list.html'
+    title = 'Это не главная страница проекта Yatube'
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Здесь будет информация о группах проекта Yatube',
+    }
+    return render(request, template, context)
+    
